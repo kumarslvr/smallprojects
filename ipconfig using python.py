5d91f7702ad1c455be7b073200b4b990c67bcdd6
kumarslvr <106430821+kumarslvr@users.noreply.github.com>
2022-06-11 17:36:41 +0530

    more modification to iponfig using jason file

--- ipconfig using python.py
+++ ipconfig using python.py
@@ -12,28 +12,25 @@ print(y)
 array1 = y.split('\r\n')
 
 dict1 = {
-        "IPv6 Address. . . . . . . . . . .": "", 
-         "Temporary IPv6 Address. . . . . .": "",
-         "Link-local IPv6 Address . . . . .": "",
-         "IPv4 Address. . . . . . . . . . .": "", 
-         "Subnet Mask . . . . . . . . . . .": "",
-         "Default Gateway . . . . . . . . .": ""
+        "IPv6 Address": "", 
+         "Temporary IPv6 Address": "",
+         "Link-local IPv6 Address": "",
+         "IPv4 Address": "", 
+         "Subnet Mask": "",
+         "Default Gateway": ""
         }
 
 
 for n in range(len(array1)):
     for i in dict1:
         if i in array1[n]:
-            if i == "Default Gateway . . . . . . . . .":
-                dict1[i.strip()] = [array1[n].split(i)[1].strip(": : "), array1[n+1].strip()]
+            if i == "Default Gateway":
+                dict1[i.strip()] = [array1[n].split(i)[1].strip(". . . . . : "), array1[n+1].strip()]
             else:
-                dict1[i.strip()] = array1[n].split(i)[1].strip(": : ")
+                dict1[i.strip()] = array1[n].split(i)[1].strip(". . . . . : ")
             
                
 print(dict1)
-# print("dict1",dict1)
-# data = json.dumps(dict1, indent = 4)
-# # print(data)
 
 with open('networkinfo.json', 'w') as f:
     json.dump(dict1, f, indent=4)
